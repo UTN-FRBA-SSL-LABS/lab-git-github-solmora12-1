73c1547c2aa806dda749dc11b55ed91995d09255
solmora12 <somora@frba.utn.edu.ar>
2026-04-19 15:40:43 -0300

    Agrega comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -14,6 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
+//Recibo dos paraeametros enteros y devuelvo el resultado de multiplicarlos
 int multiplicar(int a, int b) {
     return a * b;
 }
